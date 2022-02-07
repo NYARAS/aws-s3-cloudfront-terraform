d3c849745dea8acd6866fb41f64b8dd4e3b9cf7a
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-07 08:40:39 +0300

    fix: cleanup main.tf file
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-# module "mywebsite" {
-#   source      = "./aws-s3-website"
-#   endpoint    = "mywebsite.calvineotieno.com"
-#   domain_name = "calvineotieno.com"
-#   region      = var.region
-#   bucket_name = "mywebsite.calvineotieno.com"
-# }
-
 module "mywebsite" {
   source      = "./aws-s3-website"
   endpoint    = var.endpoint
