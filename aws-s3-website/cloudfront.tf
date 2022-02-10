d602c23299ca4c11e7d352f3374280ea79b92247
Calvine Otieno <nyarangaotieno@gmail.com>
2022-02-10 23:07:17 +0300

    feat: add cloudfront custom error response
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- aws-s3-website/cloudfront.tf
+++ aws-s3-website/cloudfront.tf
@@ -27,6 +27,11 @@ resource "aws_cloudfront_distribution" "cf" {
       }
     }
   }
+  custom_error_response {
+    error_code         = 403
+    response_code      = 200
+    response_page_path = "/index.html"
+  }
 
   restrictions {
     geo_restriction {
